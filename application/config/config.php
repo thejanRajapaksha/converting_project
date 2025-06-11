fd6159a3b774de2c09f6572e48d49dc95527f57a
thejanRajapaksha <rmhthejan31@gmail.com>
2025-06-11 20:48:45 +0530

    asx

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/erav_clothing12/';
+$config['base_url'] = 'http://localhost/converting_project/';
 
 /*
 |--------------------------------------------------------------------------
